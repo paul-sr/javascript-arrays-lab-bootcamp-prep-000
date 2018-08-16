674c2ba2d10c812912f7c552f982faaca14bfbe0
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-16 01:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ function destructivelyAppendKitten(name) {
   kittens = kittens.push(name)
   return kittens
 }
-
+function destructivelyPrependKitten(name) {
+  kittens = kittens.unshift(name)
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens = kittens.pop()
+  return kittens
+}
 function appendKitten(name) {
   return kittens.push(name)
 }
