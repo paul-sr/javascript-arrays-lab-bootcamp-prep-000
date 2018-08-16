04a34db560158aeaf654a902fc2f8d23ed33534d
saipaul@hotmail.com <saipaul@hotmail.com>
2018-08-16 00:59:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ kittens = ["Milo","Otis","Garfield"]
 
 function destructivelyAppendKitten(name) {
   kittens = kittens.push(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  return kittens.push(name)
 }
